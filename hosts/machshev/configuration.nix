fd9d82d5f778b8ec6f286ea6f341a3e3443f211b
David James McCorrie <djmccorrie@gmail.com>
2024-12-22 17:11:10 +0000

    migrate to disko

--- hosts/machshev/configuration.nix
+++ hosts/machshev/configuration.nix
@@ -1,7 +1,5 @@
 {...}: {
   imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
     ./fs.nix
     ../../config/wol.nix
   ];
@@ -18,16 +16,5 @@
     closedFirmwareUpdates = true;
   };
 
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.11";
 }
