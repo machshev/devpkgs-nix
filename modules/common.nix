72f45936f92d42819cae035a8ea85662b8b31542
David James McCorrie <djmccorrie@gmail.com>
2025-01-14 15:42:48 +0000

    add podman
    Signed-off-by: David James McCorrie <djmccorrie@gmail.com>

--- modules/common.nix
+++ modules/common.nix
@@ -28,7 +28,19 @@ with lib; {
     services.gvfs.enable = true;
     services.udisks2.enable = true;
 
-    virtualisation.docker.enable = true;
+    virtualisation.podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
+
+    # Useful other development tools
+    environment.systemPackages = with pkgs; [
+      dive # look into docker image layers
+      podman-tui # status of containers in the terminal
+      podman-compose # start group of containers for dev
+    ];
+
     virtualisation.libvirtd.enable = true;
 
     # touchpad support
