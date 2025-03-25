e103f8779221487a0c51400dc66a4afbc26d7e20
David James McCorrie <djmccorrie@gmail.com>
2025-03-25 18:32:25 +0000

    try again to fix git-forgit
    Signed-off-by: David James McCorrie <djmccorrie@gmail.com>

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -132,6 +132,11 @@
     r = "ranger";
   };
 
+  home.sessionVariables = {
+    # FORGIT_DIFF_FZF_OPTS = "--preview-window '80%'";
+    # FORGIT_DIFF_PAGER = "delta --width=$(math --scale=0 \"$(tput cols)*0.8\" )";
+  };
+
   programs.fish = {
     enable = true;
 
@@ -176,10 +181,10 @@
       pvea = ". ./.venv/bin/activate";
     };
 
-    shellInit = ''
-      set -gx FORGIT_DIFF_FZF_OPTS "--preview-window '80%'"
-      set -gx FORGIT_DIFF_PAGER "delta --width=$(math --scale=0 "$(tput cols)*0.8" )"
-    '';
+    #interactiveShellInit = ''
+    #  set -gx FORGIT_DIFF_FZF_OPTS "--preview-window '80%'"
+    #  set -gx FORGIT_DIFF_PAGER "delta --width=$(math --scale=0 "$(tput cols)*0.8" )"
+    #'';
 
     functions = {
       gcf = ''
