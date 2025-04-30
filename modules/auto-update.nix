88d1f4eb371c014c6100a1da424d8cdc9db13e61
David James McCorrie <djmccorrie@gmail.com>
2025-04-30 10:28:30 +0100

    Add an option to specify the flake to update
    Signed-off-by: David James McCorrie <djmccorrie@gmail.com>

--- modules/auto-update.nix
+++ modules/auto-update.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  inputs,
   ...
 }:
 with lib; {
@@ -11,16 +10,18 @@ with lib; {
       default = false;
       description = "Automatically update NixOS";
     };
+    machshev.autoupdate.flakePath = mkOption {
+      type = types.str;
+      description = "Path to the flake to use";
+      default = "github:machshev/mynix";
+    };
   };
 
   config = mkIf config.machshev.autoupdate.enable {
     system.autoUpgrade = {
       enable = true;
-      flake = inputs.self.outPath;
+      flake = config.machshev.autoupdate.flakePath;
       flags = [
-        "--update-input"
-        "nixpkgs"
-        "--no-write-lock-file"
         "-L" # print build logs
       ];
       dates = "02:00";
